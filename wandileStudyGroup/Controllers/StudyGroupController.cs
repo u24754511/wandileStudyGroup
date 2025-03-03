80946d691d93c84bed53aaf38b388fee2d6c0fb0
reems <reems@LAPTOP-M5VT2J8D>
2025-03-03 12:22:18 +0200

    Add project files.

---
+++ wandileStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wandileStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult StudyGroupMembers()
+        {
+
+            List<Models.StudentClass> students = new List<Models.StudentClass>();
+            {
+                students.Add(new Models.StudentClass { StudentNumber = "u24754511", FName = "Wandile", LName = "Mngomezulu", Email = "u24754511@tuks.co.za" });
+                students.Add(new Models.StudentClass { StudentNumber = "u24436729", FName = "Odaho", LName = "Mudau", Email = "Jrmudau23@gmail.com" });
+                students.Add(new Models.StudentClass { StudentNumber = "u21784532", FName = "Mary", LName = "Modiba", Email = "mummymodiba20@gmail.com" });
+                students.Add(new Models.StudentClass { StudentNumber = "u24556901", FName = "Sanele", LName = "Khanyile", Email = "KhanyileSanele90@gmail.com" });
+                students.Add(new Models.StudentClass { StudentNumber = "u24594319", FName = "Aphiwe", LName = "Mdletshe", Email = "Aphiwemdletshe267@gmail.com" });
+            }
+
+
+
+            return View(students);
+        }
+    }
+}
---
+++ wandileStudyGroup/Models/StudentClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace wandileStudyGroup.Models
+{
+	public class StudentClass
+	{
+        [Display(Name = "u24754511")]// This is a decorator. This is what should show when the Webpage loads. 
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "John")]//A decorator
+        public string FName { get; set; }
+
+        [Display(Name = "Doe")]//A decorator
+        public string LName { get; set; }
+
+        [Display(Name = "Example1@gmail.com")]//A decorator
+        public string Email { get; set; }
+
+    }
+}
